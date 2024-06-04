1de90630fee037c4f9912482a8eee53dccb0d861
DESKTOP-A2S0GEB\jsk <vldpfh548@gmail.com>
2024-06-04 15:33:46 +0900

    Add LMLT

---
+++ train_base.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Exit script if any command fails
+set -e
+
+# Base x2 re
+CUDA_VISIBLE_DEVICES=0 python3 basicsr/train.py -opt /workspace/LMLT/options/train/LMLT/train_base_DF2K_X2.yml
+
+# base x3 re
+CUDA_VISIBLE_DEVICES=0 python3 basicsr/train.py -opt /workspace/LMLT/options/train/LMLT/train_base_DF2K_X3.yml
+
+# base x4 re
+CUDA_VISIBLE_DEVICES=0 python3 basicsr/train.py -opt /workspace/LMLT/options/train/LMLT/train_base_DF2K_X4.yml
